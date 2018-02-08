cfdabb2e17a7bf03ed1a63ef1ccb0c237c0ebe53
electricessence <oren@electrified.net>
2018-02-08 11:32:02 -0800

    reformat

--- ExpressiveCommandBase.Param.cs
+++ ExpressiveCommandBase.Param.cs
@@ -11,9 +11,9 @@ namespace Open.Database.Extensions
 	{
 #pragma warning disable CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
 #pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
-							  /// <summary>
-							  /// A struct that represents the param to be created when the command is exectued.
-							  /// </summary>
+		/// <summary>
+		/// A struct that represents the param to be created when the command is exectued.
+		/// </summary>
 		public struct Param
 #pragma warning restore CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
 #pragma warning restore CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
